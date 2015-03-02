009e5ceb329011fb66b318c0b12a59088883867e
islandof <islandof@vip.qq.com>
2015-03-02 19:59:41 +0800

    add car position

---
+++ Samples/XLabs.Sample/4.Pages/Monitor/LocalsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XLabs.Sample.Pages.Monitor
+{
+    public partial class LocalsPage : ContentPage
+    {
+        public LocalsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
