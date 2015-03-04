ae534442a77a1f88f0dafcb4f68b4329d6fa93fc
islandof <156872@qq.com>
2015-03-04 10:34:22 +0800

    * Icon-60@2x.png: change the icon file change the files name
    * XLabs.sln:
    * icon80.jpg:
    * icon76.jpg:
    * icon152.jpg:
    * icon120.png:
    * icon120.jpg:
    * XLabs.Sample.iOS.csproj:
    * SijiPage.xaml:
    * SijiList.xaml:
    * SijiViewModel.cs:
    * QicheViewModel.cs:
    * icon_120x120.png:

--- Samples/XLabs.Sample/3.ViewModel/QicheViewModel.cs
+++ Samples/XLabs.Sample/3.ViewModel/QicheViewModel.cs
@@ -12,100 +12,97 @@ using Qiche = XLabs.Sample.Model.Qiche;
 
 namespace XLabs.Sample.ViewModel
 {
-    public class QicheViewModel : Forms.Mvvm.ViewModel
-    {
-        public ICommand NavigateToDetail { private set; get; }
+	public class QicheViewModel : Forms.Mvvm.ViewModel
+	{
+		public ICommand NavigateToDetail { private set; get; }
 
-        public ICommand NavigateToTrace { private set; get; }
+		public ICommand NavigateToTrace { private set; get; }
 
-        public QicheViewModel(Qiche item)
-        {
-            //	_dangerDrive = zhalanAlarm;
+		public QicheViewModel (Qiche item)
+		{
+			//	_dangerDrive = zhalanAlarm;
 
-            qicheid = item.qicheid;
-            tboxid = item.tboxid;
-            vin = item.vin;
-            chepaino = item.chepaino;
-            brandid = item.brandid;
-            brandname = item.brandname;
-            seriesid = item.seriesid;
-            seriesname = item.seriesname;
-            styleid = item.styleid;
-            stylename = item.stylename;
-            sijiid = item.sijiid;
-            sijiname = item.sijiname;
-            initavgpenyou = item.initavgpenyou;
-            if (!string.IsNullOrEmpty(item.baoxianlimit))
-            {
-                baoxianlimit = item.baoxianlimit.Replace("T", " ");    
-            }            
-            xingshizhenglimit = item.xingshizhenglimit;
-            currentlocationx = item.currentlocationx;
-            currentlocationy = item.currentlocationy;
-            currentspeed = item.currentspeed;
-            currentdirect = item.currentdirect;
-            if (!string.IsNullOrEmpty(item.createtime))
-            {
-                createtime = item.createtime.Replace("T", " ");
-            }            
-            ownercompanyid = item.ownercompanyid;
-            ownercompanyname = item.ownercompanyname;
-            if (!string.IsNullOrEmpty(item.lastactiontime))
-            {
-                lastactiontime = item.lastactiontime.Replace("T", " ");
-            }
+			qicheid = item.qicheid;
+			tboxid = item.tboxid;
+			vin = item.vin;
+			chepaino = item.chepaino;
+			brandid = item.brandid;
+			brandname = item.brandname;
+			seriesid = item.seriesid;
+			seriesname = item.seriesname;
+			styleid = item.styleid;
+			stylename = item.stylename;
+			sijiid = item.sijiid;
+			sijiname = item.sijiname;
+			initavgpenyou = item.initavgpenyou;
+			if (!string.IsNullOrEmpty (item.baoxianlimit)) {
+				baoxianlimit = item.baoxianlimit.Replace ("T", " ").Substring (0, 10);    
+			}            
+			xingshizhenglimit = item.xingshizhenglimit;
+			currentlocationx = item.currentlocationx;
+			currentlocationy = item.currentlocationy;
+			currentspeed = item.currentspeed;
+			currentdirect = item.currentdirect;
+			if (!string.IsNullOrEmpty (item.createtime)) {
+				createtime = item.createtime.Replace ("T", " ");
+			}            
+			ownercompanyid = item.ownercompanyid;
+			ownercompanyname = item.ownercompanyname;
+			if (!string.IsNullOrEmpty (item.lastactiontime)) {
+				lastactiontime = item.lastactiontime.Replace ("T", " ");
+			}
 
-            this.NavigateToDetail = new Command(() => MessagingCenter.Send(this, ""));
+			this.NavigateToDetail = new Command (() => MessagingCenter.Send (this, ""));
 
-            this.NavigateToTrace =
-                new Command(() => MessagingCenter.Send(this, "XingChengTrace"));
+			this.NavigateToTrace =
+                new Command (() => MessagingCenter.Send (this, "XingChengTrace"));
 
-        }
+		}
 
-        public string qicheid { get; set; }
+		public string qicheid { get; set; }
 
-        public string tboxid { get; set; }
+		public string tboxid { get; set; }
 
-        public string vin { get; set; }
+		public string vin { get; set; }
 
-        public string chepaino { get; set; }
+		public string chepaino { get; set; }
 
-        public string brandid { get; set; }
+		public string brandid { get; set; }
 
-        public string brandname { get; set; }
+		public string brandname { get; set; }
 
-        public string seriesid { get; set; }
+		public string seriesid { get; set; }
 
-        public string seriesname { get; set; }
+		public string seriesname { get; set; }
 
-        public string styleid { get; set; }
+		public string styleid { get; set; }
 
-        public string stylename { get; set; }
+		public string stylename { get; set; }
 
-        public string sijiid { get; set; }
+		public string sijiid { get; set; }
 
-        public string sijiname { get; set; }
+		public string sijiname { get; set; }
 
-        public string initavgpenyou { get; set; }
+		public string initavgpenyou { get; set; }
 
-        public string baoxianlimit { get; set; }
+		public string baoxianlimit { get; set; }
 
-        public string xingshizhenglimit { get; set; }
+		public string xingshizhenglimit { get; set; }
 
-        public string currentlocationx { get; set; }
+		public string currentlocationx { get; set; }
 
-        public string currentlocationy { get; set; }
+		public string currentlocationy { get; set; }
 
-        public string currentspeed { get; set; }
+		public string currentspeed { get; set; }
 
-        public string currentdirect { get; set; }
+		public string currentdirect { get; set; }
 
-        public string createtime { get; set; }
+		public string createtime { get; set; }
 
-        public string ownercompanyid { get; set; }
+		public string ownercompanyid { get; set; }
 
-        public string ownercompanyname { get; set; }
+		public string ownercompanyname { get; set; }
 
-        public string lastactiontime { get; set; }
-    }
+		public string lastactiontime { get; set; }
+	}
 }
--- Samples/XLabs.Sample/3.ViewModel/SijiViewModel.cs
+++ Samples/XLabs.Sample/3.ViewModel/SijiViewModel.cs
@@ -12,56 +12,57 @@ using XLabs.Sample.Model;
 
 namespace XLabs.Sample.ViewModel
 {
-    public class SijiViewModel : Forms.Mvvm.ViewModel
-    {
-        private readonly IDevice _device;
+	public class SijiViewModel : Forms.Mvvm.ViewModel
+	{
+		private readonly IDevice _device;
 
-        public ICommand NavigateToDetail { private set; get; }
+		public ICommand NavigateToDetail { private set; get; }
 
-        public ICommand CallCommand { private set; get; }
+		public ICommand CallCommand { private set; get; }
 
-        public SijiViewModel(Siji item)
-        {
-            //	_dangerDrive = zhalanAlarm;
-            _device = Resolver.Resolve<IDevice>();
-            sijiid = item.sijiid;
-            sijiname = item.sijiname;
-            lianxidianhua = item.lianxidianhua;
-            if (!string.IsNullOrEmpty(item.jiashizhengriqi))
-            {
-                jiashizhengriqi = item.jiashizhengriqi.Replace("T", " ");
-            }
-            if (!string.IsNullOrEmpty(item.createtime))
-            {
-                createtime = item.createtime.Replace("T", " ");
-            }
-            companyid = item.companyid;
-            ownercompanyid = item.ownercompanyid;
-            ownercompanyname = item.ownercompanyname;
+		public SijiViewModel (Siji item)
+		{
+			//	_dangerDrive = zhalanAlarm;
+			_device = Resolver.Resolve<IDevice> ();
+			sijiid = item.sijiid;
+			sijiname = item.sijiname;
+			lianxidianhua = item.lianxidianhua;
+			if (!string.IsNullOrEmpty (item.jiashizhengriqi)) {
+				jiashizhengriqi = item.jiashizhengriqi.Replace ("T", " ").Substring (0, 10);
+				;
+			}
+			if (!string.IsNullOrEmpty (item.createtime)) {
+				createtime = item.createtime.Replace ("T", " ");
+			}
+			companyid = item.companyid;
+			ownercompanyid = item.ownercompanyid;
+			ownercompanyname = item.ownercompanyname;
 
-            this.NavigateToDetail = new Command(() => MessagingCenter.Send(this, ""));
+			extdetail = item.ownercompanyname + " " + item.lianxidianhua;
+			this.NavigateToDetail = new Command (() => MessagingCenter.Send (this, ""));
 
-            this.CallCommand = new Command(
-                () => _device.PhoneService.DialNumber(lianxidianhua),
-                () => _device.PhoneService != null);
+			this.CallCommand = new Command (
+				() => _device.PhoneService.DialNumber (lianxidianhua),
+				() => _device.PhoneService != null);
 
-        }
+		}
 
-        public string sijiid { get; set; }
+		public string sijiid { get; set; }
 
-        public string sijiname { get; set; }
+		public string sijiname { get; set; }
 
-        public string lianxidianhua { get; set; }
+		public string lianxidianhua { get; set; }
 
-        public string jiashizhengriqi { get; set; }
+		public string jiashizhengriqi { get; set; }
 
-        public string createtime { get; set; }
+		public string createtime { get; set; }
 
-        public string companyid { get; set; }
+		public string companyid { get; set; }
 
-        public string ownercompanyid { get; set; }
+		public string ownercompanyid { get; set; }
 
-        public string ownercompanyname { get; set; }
+		public string ownercompanyname { get; set; }
 
-    }
+		public string extdetail { get; set; }
+	}
 }
